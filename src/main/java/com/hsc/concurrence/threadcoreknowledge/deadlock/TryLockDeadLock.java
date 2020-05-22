d6e1e52e02a8f5ac7529cb3d9071de2539b17367
housenchuan <282628181@qq.com>
2020-05-22 20:21:41 +0800

    死锁下

---
+++ src/main/java/com/hsc/concurrence/threadcoreknowledge/deadlock/TryLockDeadLock.java
@@ -0,0 +1,70 @@
+package com.hsc.concurrence.threadcoreknowledge.deadlock;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 避免死锁 设置超时时间 但是synchronized不支持超时时间设置
+ */
+public class TryLockDeadLock implements Runnable{
+    int flag = 0;
+    static Lock lock1 = new ReentrantLock();
+    static Lock lock2 = new ReentrantLock();
+
+    public static void main(String[] args) {
+        TryLockDeadLock task1 = new TryLockDeadLock();
+        TryLockDeadLock task2 = new TryLockDeadLock();
+        task1.flag = 0;
+        task2.flag = 1;
+        new Thread(task1).start();
+        new Thread(task2).start();
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                if (flag == 0) {
+                    if (lock1.tryLock(800, TimeUnit.MILLISECONDS)) {
+                        System.out.println(Thread.currentThread().getName() + "获取lock1成功");
+                        Thread.sleep(4000);
+                        if (lock2.tryLock(800, TimeUnit.MILLISECONDS)) {
+                            System.out.println(Thread.currentThread().getName() + "获取lock2成功");
+                            Thread.sleep(400);
+                            lock2.unlock();
+                            lock1.unlock();
+                            break;
+                        } else {
+                            System.out.println(Thread.currentThread().getName() + "获取lock2失败，释放已经得到的lock1,并准备重试");
+                            lock1.unlock();
+                        }
+                    } else {
+                        System.out.println(Thread.currentThread().getName() + "获取lock1失败,准备重试");
+                    }
+                }
+                if (flag == 1) {
+                    if (lock2.tryLock(6000, TimeUnit.MILLISECONDS)) {
+                        System.out.println(Thread.currentThread().getName() + "获取lock2成功");
+                        Thread.sleep(200);
+                        if (lock1.tryLock(6000, TimeUnit.MILLISECONDS)) {
+                            System.out.println(Thread.currentThread().getName() + "获取lock1成功");
+                            Thread.sleep(200);
+                            lock1.unlock();
+                            lock2.unlock();
+                            break;
+                        } else {
+                            System.out.println(Thread.currentThread().getName() + "获取lock1失败，释放已经得到的lock2,并准备重试");
+                            lock2.unlock();
+                        }
+                    } else {
+                        System.out.println(Thread.currentThread().getName() + "获取lock2失败,准备重试");
+                    }
+                }
+            }
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/hsc/concurrence/threadcoreknowledge/deadlock/resolvedeadlock/LiveLockDeadLock.java
@@ -0,0 +1,79 @@
+package com.hsc.concurrence.threadcoreknowledge.deadlock.resolvedeadlock;
+
+import java.util.Random;
+
+/**
+ * 活锁 -- 线程一直在运行，但是一直没有进展一直在做重复事情
+ */
+public class LiveLockDeadLock {
+
+    static class Spoon{
+        private Diner owner;
+
+        public synchronized void use(){
+            System.out.printf("%s has eaten",owner.name);System.out.println();
+        }
+
+        public Spoon(Diner owner) {
+            this.owner = owner;
+        }
+
+        public Diner getOwner() {
+            return owner;
+        }
+
+        public void setOwner(Diner owner) {
+            this.owner = owner;
+        }
+    }
+    static class Diner{
+        public Diner(String name) {
+            this.name = name;
+        }
+
+        private String name;
+        private boolean isHungry;
+        public void eatWith(Spoon spoon, Diner spouse) throws InterruptedException {
+            while (isHungry){
+                if(spoon.owner != this){
+                    Thread.sleep(10);
+                    continue;
+                }
+                Random random = new Random();
+                if(spouse.isHungry && random.nextInt(10) < 9){
+                    System.out.printf("%s 你先吃",spouse.name);System.out.println();
+                    spoon.setOwner(spouse);
+                    continue;
+                }
+                spoon.use();
+                isHungry = false;
+                spoon.setOwner(spouse);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Diner niulang = new Diner("牛郎");
+        niulang.isHungry = true;
+        Diner zhinv = new Diner("织女");
+        zhinv.isHungry = true;
+        Spoon spoon = new Spoon(niulang);
+        Runnable taskOne = ()->{
+            try {
+                niulang.eatWith(spoon,zhinv);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        Runnable taskTwo = ()->{
+            try {
+                zhinv.eatWith(spoon,niulang);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        new Thread(taskOne).start();
+        new Thread(taskTwo).start();
+
+    }
+}
