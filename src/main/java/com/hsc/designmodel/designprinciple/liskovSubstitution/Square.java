723e2af75536768123552fa430369bbc629df28e
housenchuan <senchuan.hou@jieshunpay.cn>
2020-06-02 20:30:19 +0800

    设计模式

---
+++ src/main/java/com/hsc/designmodel/designprinciple/liskovSubstitution/Square.java
@@ -0,0 +1,17 @@
+package com.hsc.designmodel.designprinciple.liskovSubstitution;
+
+/**
+ * 正方形
+ **/
+
+public class Square {
+    private int sideLength;
+
+    public int getSideLength() {
+        return sideLength;
+    }
+
+    public void setSideLength(int sideLength) {
+        this.sideLength = sideLength;
+    }
+}
---
+++ src/main/java/com/hsc/designmodel/designprinciple/liskovSubstitution/Test.java
@@ -0,0 +1,21 @@
+package com.hsc.designmodel.designprinciple.liskovSubstitution;
+
+/**
+ **/
+
+public class Test {
+
+    public static void resize(Rectangle rectangle){
+        while (rectangle.getWidth() <= rectangle.getLength()){
+            rectangle.setWidth(rectangle.getWidth()+1);
+            System.out.println("width="+rectangle.getWidth()+"   length="+rectangle.getLength());
+        }
+        System.out.println("方法结束width="+rectangle.getWidth()+"   length="+rectangle.getLength());
+    }
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        rectangle.setWidth(10);
+        rectangle.setLength(20);
+        resize(rectangle);
+    }
+}
