4f6146cf38eaab48db4b437fc97d059c8cbddc67
housenchuan <282628181@qq.com>
2021-11-23 08:14:15 +0800

    链表反转 二分法查找

--- src/main/java/com/hsc/algorithm/ArrayPractice.java
+++ src/main/java/com/hsc/algorithm/ArrayPractice.java
@@ -1,5 +1,6 @@
 package com.hsc.algorithm;
 
+
 public class ArrayPractice {
 
 
@@ -59,13 +60,57 @@ public class ArrayPractice {
         return max;
     }
 
+    /**
+     * 二分法查找--前提是数组是有序的
+     * 思路：设left和right分别为数组的开始和结束位置，判断mid是否等于target值，若小于则往数组的
+     *      左边进行寻找，若大于则往数组的右边进行寻找
+     * 时间复杂度：O(logn)
+     * 空间复杂度: O(1)
+     * @param nums 待查找数组--数组是升序的
+     * @param target 目标值
+     * @return 目标值的下标
+     */
+    public static int search(int nums[],int target){
+        for (int left = 0,right = nums.length-1; left <= right;) {
+            int mid = (right - left)/2 + left;
+            if(nums[mid] == target){
+                return mid;
+            }else if(target > nums[mid]){
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 字符串相加
+     * @param num1
+     * @param num2
+     * @return 字符串相加的结果
+     */
+    public static String addStrings(String num1, String num2){
+        StringBuilder sb = new StringBuilder("");
+        int i = num1.length()-1;
+        int j = num2.length()-1;
+        int carry = 0;
+        while (i>=0 || j>=0 || carry!=0){
+            int n1 = i>=0?num1.charAt(i)-'0':0;
+            int n2 = j>=0?num2.charAt(j)-'0':0;
+            int tmp = n1+n2+carry;
+            carry = tmp/10;
+            sb.append(tmp%10);
+            i--;
+            j--;
+        }
+        return sb.reverse().toString();
+    }
+
     public static void main(String[] args) {
-//        int arr[] = {5,4,3,2,1};
-//        quickSort(arr,0,arr.length-1);
-//        for (int num : arr) {
-//            System.out.println(num);
-//        }
-        int arr[] = {1,7,3,4,5};
-        System.out.println(maxProfits(arr));
+        int arr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+        String num1 = "7234";
+        String num2 = "7237"; //02471
+        System.out.println(addStrings(num1,num2));
     }
 }
---
+++ src/main/java/com/hsc/algorithm/LinkedPractice.java
@@ -0,0 +1,52 @@
+package com.hsc.algorithm;
+
+public class LinkedPractice {
+
+    /**
+     * 反转链表2.0
+     * @param head 待反转链表
+     * @return 饭转后的链表
+     */
+    public static Node reverseLinked(Node head){
+        Node pre = null;
+        Node cur = head;
+        while (cur != null){
+            Node temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+    public static void main(String[] args) {
+        Node head = new Node(1,null);
+        Node nodeOne = new Node(2,null);
+        head.next = nodeOne;
+        Node nodeTwo = new Node(3,null);
+        nodeOne.next = nodeTwo;
+        Node nodeThree = new Node(4,null);
+        nodeTwo.next = nodeThree;
+        Node nodeFour = new Node(5,null);
+        nodeThree.next = nodeFour;
+
+        Node resultNode =  reverseLinked(head);
+        Node node = resultNode;
+        while (node != null){
+            System.out.println(node.value);
+            node = node.next;
+        }
+        System.out.println();
+
+    }
+
+}
+
+class Node{
+    int value;
+    Node next;
+
+    public Node(int value,Node next){
+        this.value = value;
+        this.next = next;
+    }
+}
