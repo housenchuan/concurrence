cce12fb5d46b5731913fdec006f28890cc81ad42
housenchuan <282628181@qq.com>
2022-02-04 18:49:23 +0800

    算法

--- src/main/java/com/hsc/algorithm/string/StringPractice.java
+++ src/main/java/com/hsc/algorithm/string/StringPractice.java
@@ -183,6 +183,16 @@ public class StringPractice {
         return null;
     }
 
+
+    /**
+     * 子集
+     * 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+     * 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+     */
+    public static List<List<Integer>> subsets(int [] nums){
+        return null;
+    }
+
     public static void main(String[] args) {
         System.out.println(longestHwStr_2("babad"));
     }
